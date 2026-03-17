416e80ef4a4ec242117dcb9fb3a798220379dd5b
Каракулло Лев <CrossoutGamer226@gmail.com>
2026-03-17 10:39:03 +0700

    Добавьте файлы проекта.

---
+++ Lab2_Vich.Mat/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Lab2_Vich.Mat
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.colY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить графики";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(513, -2);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(704, 695);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // colY
+            // 
+            this.colY.HeaderText = "Y";
+            this.colY.MinimumWidth = 6;
+            this.colY.Name = "colY";
+            this.colY.Width = 125;
+            // 
+            // colX
+            // 
+            this.colX.HeaderText = "X";
+            this.colX.MinimumWidth = 6;
+            this.colX.Name = "colX";
+            this.colX.Width = 125;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colX,
+            this.colY});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(467, 273);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 705);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Аппроксимация функций";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colX;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Lab2_Vich.Mat/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab2_Vich.Mat
+{
+    public partial class Form1 : Form
+    {
+        List<double> X = new List<double>();
+        List<double> Y = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillDefaultPoints();
+        }
+
+        // ---------------- ЗАПОЛНЕНИЕ ТАБЛИЦЫ (вариант 5) ----------------
+        void FillDefaultPoints()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add("colX", "X");
+            dataGridView1.Columns.Add("colY", "Y");
+
+            double[] X0 = { 5, 7, 9, 11, 12 };
+            double[] Y0 = { 3, -2, -2, 4, 15 };
+
+            for (int i = 0; i < X0.Length; i++)
+                dataGridView1.Rows.Add(X0[i], Y0[i]);
+        }
+
+        // ---------------- ЧТЕНИЕ ТАБЛИЦЫ ----------------
+        void ReadTable()
+        {
+            X.Clear();
+            Y.Clear();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value == null || row.Cells[1].Value == null)
+                    continue;
+
+                X.Add(Convert.ToDouble(row.Cells[0].Value));
+                Y.Add(Convert.ToDouble(row.Cells[1].Value));
+            }
+        }
+
+        // ---------------- ИНТЕРПОЛЯЦИОННЫЙ МНОГОЧЛЕН (через твой Гаусс) ----------------
+        double[] InterpolationPolynomial()
+        {
+            int n = X.Count;
+
+            float[,] A = new float[n, n];
+            float[] b = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                float val = 1f;
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = val;
+                    val *= (float)X[i];
+                }
+                b[i] = (float)Y[i];
+            }
+
+            float[] res = GaussMethod.SelectByLine(A, b);
+
+            double[] result = new double[n];
+            for (int i = 0; i < n; i++)
+                result[i] = res[i];
+
+            return result;
+        }
+
+        double PolyEval(double[] a, double x)
+        {
+            double s = 0;
+            double p = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i] * p;
+                p *= x;
+            }
+            return s;
+        }
+
+        // ---------------- ЛАГРАНЖ ----------------
+        double Lagrange(double x)
+        {
+            double sum = 0;
+            int n = X.Count;
+
+            for (int k = 0; k < n; k++)
+            {
+                double lk = 1;
+                for (int j = 0; j < n; j++)
+                    if (j != k)
+                        lk *= (x - X[j]) / (X[k] - X[j]);
+
+                sum += Y[k] * lk;
+            }
+            return sum;
+        }
+
+        // ---------------- НЬЮТОН ----------------
+        double[] NewtonCoeffs()
+        {
+            int n = X.Count;
+            double[,] dd = new double[n, n];
+            double[] a = new double[n];
+
+            for (int i = 0; i < n; i++)
+                dd[i, 0] = Y[i];
+
+            for (int j = 1; j < n; j++)
+                for (int i = 0; i < n - j; i++)
+                    dd[i, j] = (dd[i + 1, j - 1] - dd[i, j - 1]) / (X[i + j] - X[i]);
+
+            for (int i = 0; i < n; i++)
+                a[i] = dd[0, i];
+
+            return a;
+        }
+
+        double NewtonEval(double x, double[] a)
+        {
+            double result = a[0];
+            double prod = 1;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                prod *= (x - X[i - 1]);
+                result += a[i] * prod;
+            }
+            return result;
+        }
+
+        // ---------------- ПОСТРОЕНИЕ ГРАФИКОВ ----------------
+        void Plot()
+        {
+            ReadTable();
+            chart1.Series.Clear();
+
+            var sPoints = chart1.Series.Add("Точки");
+            sPoints.ChartType = SeriesChartType.Point;
+            for (int i = 0; i < X.Count; i++)
+                sPoints.Points.AddXY(X[i], Y[i]);
+
+            double xmin = X.Min();
+            double xmax = X.Max();
+            List<double> grid = Enumerable.Range(0, 300)
+                .Select(i => xmin + i * (xmax - xmin) / 299.0).ToList();
+
+            var aInterp = InterpolationPolynomial();
+            var sInterp = chart1.Series.Add("Интерполяционный многочлен");
+            sInterp.ChartType = SeriesChartType.Line;
+            foreach (double x in grid)
+                sInterp.Points.AddXY(x, PolyEval(aInterp, x));
+
+            var sLag = chart1.Series.Add("Лагранж");
+            sLag.ChartType = SeriesChartType.Line;
+            foreach (double x in grid)
+                sLag.Points.AddXY(x, Lagrange(x));
+
+            var aNewton = NewtonCoeffs();
+            var sNewt = chart1.Series.Add("Ньютон");
+            sNewt.ChartType = SeriesChartType.Line;
+            foreach (double x in grid)
+                sNewt.Points.AddXY(x, NewtonEval(x, aNewton));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Plot();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void chart1_Click(object sender, EventArgs e) { }
+    }
+
+    // ---------------- ТВОЙ МЕТОД ГАУССА ----------------
+    class GaussMethod
+    {
+        public static float[] SelectByLine(float[,] A, float[] b)
+        {
+            int n = b.Length;
+            float[,] a = (float[,])A.Clone();
+            float[] bb = (float[])b.Clone();
+            int[] colIndex = new int[n];
+
+            for (int i = 0; i < n; i++)
+                colIndex[i] = i;
+
+            for (int k = 0; k < n - 1; k++)
+            {
+                int maxCol = k;
+                float maxVal = Math.Abs(a[k, k]);
+
+                for (int j = k + 1; j < n; j++)
+                    if (Math.Abs(a[k, j]) > maxVal)
+                    {
+                        maxVal = Math.Abs(a[k, j]);
+                        maxCol = j;
+                    }
+
+                if (maxCol != k)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        float temp = a[i, k];
+                        a[i, k] = a[i, maxCol];
+                        a[i, maxCol] = temp;
+                    }
+
+                    int tmpIdx = colIndex[k];
+                    colIndex[k] = colIndex[maxCol];
+                    colIndex[maxCol] = tmpIdx;
+                }
+
+                for (int m = k + 1; m < n; m++)
+                {
+                    float factor = a[m, k] / a[k, k];
+                    for (int l = k; l < n; l++)
+                        a[m, l] -= factor * a[k, l];
+                    bb[m] -= factor * bb[k];
+                }
+            }
+
+            float[] xTemp = new float[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                float sum = 0;
+                for (int j = i + 1; j < n; j++)
+                    sum += a[i, j] * xTemp[j];
+                xTemp[i] = (bb[i] - sum) / a[i, i];
+            }
+
+            float[] x = new float[n];
+            for (int i = 0; i < n; i++)
+                x[colIndex[i]] = xTemp[i];
+
+            return x;
+        }
+    }
+}
